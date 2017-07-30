f35629db0011b51b79e9238cc2870cfbd418ca3f
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-07-30 10:28:33 +0200

    in 'score(game)': used 'enumerate()', used 'lower()'

--- bowling.py
+++ bowling.py
@@ -2,40 +2,38 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
+    for idx, char in enumerate(game):  # enumerate()
+        if char == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+            result += get_value(char)
+        if frame < 10 and get_value(char) == 10:
+            if char == '/':
+                result += get_value(game[idx+1])
+            elif char.lower() == 'x':  # lower()
+                result += get_value(game[idx+1])
+                if game[idx+2] == '/':
+                    result += 10 - get_value(game[idx+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result += get_value(game[idx+2])
+        last = get_value(char)
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if char.lower() == 'x':  # lower()
             in_first_half = True
             frame += 1
     return result
 
 
 def get_value(char):
-    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']  # created a list of numbers 1-9
+    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']  # list of numbers 1-9
     if char in nums:
         return int(char)
-    elif char.lower() == 'x':  # used 'lower()' method
+    elif char.lower() == 'x':  # lower()
         return 10
     elif char == '/':
         return 10
